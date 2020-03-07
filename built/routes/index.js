0295d64b45570c9a1baafd43548b25853079bc35
opalcat <noblesse3539@daum.net>
2020-03-07 22:08:20 +0900

    Apply .gitignore

--- built/routes/index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.send('hello!!!');
-});
-module.exports = router;
